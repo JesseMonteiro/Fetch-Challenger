9ec26e425c843eb21db8bb443bd14be247253cd9
Jessé Monteiro <jessemonteiroferreira@gmail.com>
2024-09-05 00:22:36 -0300

    Initial Commit

---
+++ Fetch Challenger/Fetch_ChallengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_ChallengerApp.swift
+//  Fetch Challenger
+//
+//  Created by Jesse Monteiro Ferreira on 05/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_ChallengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
